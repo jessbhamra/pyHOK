eca0140b838e460ac69f86993b2ef6e2d3b882aa
jessbhamra <38768671+jessbhamra@users.noreply.github.com>
2024-02-12 14:54:45 -0500

    Update script.py
    ok this one was totally changed up to do all of the type manipulations in the FAMILY EDITOR instead of in the project document. Still needs the purge function worked out.

--- Doors.extension/pyHOK.tab/HOK Tools.panel/Door Configurator.pushbutton/script.py
+++ Doors.extension/pyHOK.tab/HOK Tools.panel/Door Configurator.pushbutton/script.py
@@ -8,6 +8,7 @@ from pyrevit import forms, revit
 import tempfile
 import os
 import clr
+import System
 
 __context__ = 'Doors'
 
@@ -17,7 +18,7 @@ ui = __revit__.ActiveUIDocument
 doorm = ui.Selection.GetElementIds()
 
 # Get the element selection of the current document
-# replace this with something hard coded to pull prototype door- put it on github as part of extension
+# replace this with something hard coded to pull prototype door
 doorid = (doorm[0])
 door= doc.GetElement(doorid)
 
@@ -38,22 +39,103 @@ def save_as_new_family(door, family_name, panel_type, frame_type, width, height)
 
 # Edit Family to bring up the family editor
     family_temp = (doc.EditFamily(fam_tpe))#EditFamily must be called OUTSIDE of a transaction
+#make new type and assign values
+    typeName = "{}x{}".format(int(width*12), int(height*12))
 
-# save as family_temp as doc to find nested panels and frames
+
+#start a transaction and instantiate family manager
+    with Transaction(family_temp, 'Make Type and set Values') as trans:
+        trans.Start()
+        famMan = family_temp.FamilyManager
+        famFamily = family_temp.OwnerFamily
+        print ("51" , (famFamily))
+        typeMake = famMan.NewType(typeName)
+
+        
+#these are the shared parameter GUIDs for the parameters we're looking for
+        pwGU = "318d67dd-1f5f-43fb-a3d0-32ac31f1babb"#PANEL WIDTH PANEL 1
+        phGU = "3e7f226e-bc78-407c-982a-d45a405cd2a9"#PANEL HEIGHT
+        pnGU = "8e89f65d-3ed9-45c8-9808-8c315dedadce" #PANEL 1
+        pfGU = "b6930f0e-c0f5-432b-80ee-6c649f876cae" #FRAME
+
+
+  #filtered element collector to grab nested door families
+        nestFams = DB.FilteredElementCollector(family_temp)\
+                .OfClass(DB.Family).ToElements()
+                
+        BamId = None
+        FamId = None                
+     
+        for FamThis in nestFams:
+            if FamThis.Name == panel_type:
+                BamId = FamThis
+            elif FamThis.Name == frame_type:
+                FamId = FamThis
+                
+#If a new type was made successfully
+        if typeMake:
+#get the set of Family parameters and iterate through them
+            paraSet = famMan.GetParameters()
+            #for each parameter in the family
+            for parr in paraSet:
+                #if the parameter is shared,
+                if parr.IsShared:
+                    pParr = parr.GUID
+                    #print ( pParr)
+
+  #check if the parameter GUID matches the 4 parameters we are looking to set
+  #                   
+                    if str(pParr) == pwGU:
+                    #if PANEL WIDTH PANEL 
+                        famMan.Set(parr, width)
+                    
+                    elif str(pParr) == phGU:
+                    #elif PANEL HEIGHT   
+                        famMan.Set(parr, height)
+
+                    elif str(pParr) == pnGU:
+                        print (BamId.Id)
+                        BamSyms = BamId.GGetFamilySymbolIds()
+                        for BamSym in BamSyms:
+                            famMan.Set(parr, BamSym) 
+                            print(str(BamSym))
+                    
+                    elif str(pParr) == pfGU:
+                        FamSyms = FamId.GetFamilySymbolIds()
+                        print(FamSyms)
+                        for FamSym in FamSyms:
+                            famMan.Set(parr, (FamSym)) 
+                            
+
+            trans.Commit()
+
+        else:
+            print ("88")
+            trans.RollBack()
+
+
+#delete Delete family
+
+#save as the family with new name and path
     family_temp.SaveAs(family_path, DB.SaveAsOptions())
 
 # Load the saved family back into the project
     with Transaction(doc, 'Load Family') as trans:
         trans.Start()
         family_loaded= doc.LoadFamily(family_path)
-        print (str(family_loaded))
+       # print (str(family_loaded))
         if not family_loaded:
             print("Failed to load family.")
             return
         trans.Commit()
 
-# Find the loaded family by name to work with its symbols
+    # Clean up the temporary directory
+    os.remove(family_path)
+    os.rmdir(backupf_path)
+    os.rmdir(temp_dir)
 
+def edit_types_and_params(family_name, panel_type, frame_type, width, height):
+# Find the loaded family by name to work with its symbols
 # open a transaction to make changes to things in Revit
     with Transaction(doc, 'Create New Family Type') as trans:
         trans.Start()
@@ -69,7 +151,7 @@ def save_as_new_family(door, family_name, panel_type, frame_type, width, height)
 
                 for symbol_id in family_symbols:
                     symbol = doc.GetElement(symbol_id)
-                    # Assuming you want to duplicate the first symbol for simplicity
+                    #duplicate the first symbol for simplicity
                     #specify which to duplicate
                     new_symbol_id = symbol.Duplicate("{}x{}".format(int(width*12), int(height*12)))
                     new_sym_ref = DB.Reference(new_symbol_id)
@@ -111,10 +193,10 @@ def save_as_new_family(door, family_name, panel_type, frame_type, width, height)
                             break
                     FramElem = (doc.GetElement(FramId))
                     new_symbol.LookupParameter('FRAME').Set(FramElem.Id)
-
+###########try to do this another way using Type instead of symbol
                     # Rename the family symbol to reflect the new dimensions in inches
                     new_symbol.Name = "{}x{}".format(int(width*12), int(height*12))
-
+#this may have to be done in the purge function instead
                     #delete the Delete type
                     delSym = doc.Delete(symbol_id)
     # call purge
@@ -122,17 +204,12 @@ def save_as_new_family(door, family_name, panel_type, frame_type, width, height)
                 break  # Exit after finding the family
         trans.Commit()
 
-    # Clean up the temporary directory
-    os.remove(family_path)
-    os.rmdir(backupf_path)
-    os.rmdir(temp_dir)
-
-def edit_types_and_params(family_name, panel_type, frame_type, width, height):
-    
 
 def call_purge(family_name):
 #Function to purge unused nested families from a specified family.
-    
+
+
+#      
     # Find the family by name
     family = None
     for el in FilteredElementCollector(doc).OfClass(DB.Family):
@@ -145,6 +222,9 @@ def call_purge(family_name):
         # Edit the family to access nested families
         with Transaction((famDoc), 'Purge Unused Nested Families') as trans:
             trans.Start()
+#Function to call Types from type symbols
+# Also delete Delete type
+# then purge
 
             try:
                 # Check nested families and delete unused ones
@@ -197,7 +277,7 @@ def main():
     #forms.ask_for_string("Enter Panel Type")
     frame_type = "S01"
     #forms.ask_for_string("Enter Frame Type")
-    width = 36
+    width = 37
     #forms.ask_for_string("Enter Width (in inches)")
     height = 84
     #forms.ask_for_string("Enter Height (in inches)")
@@ -215,10 +295,22 @@ def main():
     save_as_new_family(door, family_name, panel_type, frame_type, width, height)
 
 # Update the parameters in the door
-   # update_door_parameters(door, family_name, panel_type, frame_type, width, height)
+#    edit_types_and_params(family_name, panel_type, frame_type, width, height)
 
 #purrrrrrggggggggeeeeeee
-    call_purge(family_name)
+ #   call_purge(family_name)
 
 # Call the main function
 main()
+#elif PANEL 1  and FRAME with an else so only have to iterate through fEC once
+                        
+                     #filtered element collector to grab nested door families
+                       # parTry = DB.FilteredElementCollector(family_temp)\
+                                #.OfCategory(BuiltInCategory.OST_Doors)\
+                                #.WhereElementIsElementType()
+                        
+                        #parrGet = family_temp.GetElement(parr.Id)
+                        #famTypes = famFamily.GetFamilyTypeParameterValues(parrGet.Id)
+                        #print (famTypes, "91")
+                        #BamId = None
+                        #FamId = None
