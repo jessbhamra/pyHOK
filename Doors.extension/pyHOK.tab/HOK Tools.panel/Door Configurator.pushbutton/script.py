ece8529e967fb42f0af3440c3583b35b99ea9adf
jessbhamra <38768671+jessbhamra@users.noreply.github.com>
2024-02-13 07:45:05 -0500

    Update script.py
    ok everything is working here except the purge

--- Doors.extension/pyHOK.tab/HOK Tools.panel/Door Configurator.pushbutton/script.py
+++ Doors.extension/pyHOK.tab/HOK Tools.panel/Door Configurator.pushbutton/script.py
@@ -30,7 +30,8 @@ def save_as_new_family(door, family_name, panel_type, frame_type, width, height)
     backupf_path = os.path.join(temp_dir,"Backup")
 
 # get the prototype door family
-    print (str(door))
+    #print (str(door))
+    print("making a new family...")
     fam_lam = DB.Element.GetValidTypes(door)
     fam_nam = fam_lam[0]
     family_type = (doc.GetElement(fam_nam))
@@ -45,81 +46,110 @@ def save_as_new_family(door, family_name, panel_type, frame_type, width, height)
 
 #start a transaction and instantiate family manager
     with Transaction(family_temp, 'Make Type and set Values') as trans:
-        trans.Start()
-        famMan = family_temp.FamilyManager
-        famFamily = family_temp.OwnerFamily
-        print ("51" , (famFamily))
-        typeMake = famMan.NewType(typeName)
+        try:
+            trans.Start()
+            famMan = family_temp.FamilyManager
+            famFamily = family_temp.OwnerFamily
+            #print ("51" , (famFamily))
+            deleteType = famMan.CurrentType
+            #print(str(deleteType),  " delete bB")
+            typeMake = famMan.NewType(typeName)
+            print("making new type...")
 
         
 #these are the shared parameter GUIDs for the parameters we're looking for
-        pwGU = "318d67dd-1f5f-43fb-a3d0-32ac31f1babb"#PANEL WIDTH PANEL 1
-        phGU = "3e7f226e-bc78-407c-982a-d45a405cd2a9"#PANEL HEIGHT
-        pnGU = "8e89f65d-3ed9-45c8-9808-8c315dedadce" #PANEL 1
-        pfGU = "b6930f0e-c0f5-432b-80ee-6c649f876cae" #FRAME
+            pwGU = "318d67dd-1f5f-43fb-a3d0-32ac31f1babb"#PANEL WIDTH PANEL 1
+            phGU = "3e7f226e-bc78-407c-982a-d45a405cd2a9"#PANEL HEIGHT
+            pnGU = "8e89f65d-3ed9-45c8-9808-8c315dedadce" #PANEL 1
+            pfGU = "b6930f0e-c0f5-432b-80ee-6c649f876cae" #FRAME
 
 
   #filtered element collector to grab nested door families
-        nestFams = DB.FilteredElementCollector(family_temp)\
+            nestFams = DB.FilteredElementCollector(family_temp)\
                 .OfClass(DB.Family).ToElements()
                 
-        BamId = None
-        FamId = None                
-     
-        for FamThis in nestFams:
-            if FamThis.Name == panel_type:
-                BamId = FamThis
-            elif FamThis.Name == frame_type:
-                FamId = FamThis
+            BamId = None
+            FamId = None                
+            FlamId = None
+            print("updating parameters...")
+            for FamThis in nestFams:
+                if FamThis.Name == panel_type:
+                    BamId = FamThis
+                elif FamThis.Name == frame_type:
+                    FamId = FamThis
+                else: FlamId = FamThis
+
                 
 #If a new type was made successfully
-        if typeMake:
+            if typeMake:
 #get the set of Family parameters and iterate through them
-            paraSet = famMan.GetParameters()
+                paraSet = famMan.GetParameters()
             #for each parameter in the family
-            for parr in paraSet:
+                for parr in paraSet:
                 #if the parameter is shared,
-                if parr.IsShared:
-                    pParr = parr.GUID
+                    if parr.IsShared:
+                        pParr = parr.GUID
                     #print ( pParr)
 
   #check if the parameter GUID matches the 4 parameters we are looking to set
   #                   
-                    if str(pParr) == pwGU:
+                        if str(pParr) == pwGU:
                     #if PANEL WIDTH PANEL 
-                        famMan.Set(parr, width)
+                            famMan.Set(parr, width)
                     
-                    elif str(pParr) == phGU:
+                        elif str(pParr) == phGU:
                     #elif PANEL HEIGHT   
-                        famMan.Set(parr, height)
+                            famMan.Set(parr, height)
 
-                    elif str(pParr) == pnGU:
-                        print (BamId.Id)
-                        BamSyms = BamId.GGetFamilySymbolIds()
-                        for BamSym in BamSyms:
-                            famMan.Set(parr, BamSym) 
-                            print(str(BamSym))
+                        elif str(pParr) == pnGU:
+                            #print (BamId.Id)
+                            BamSyms = BamId.GetFamilySymbolIds()
+                            for BamSym in BamSyms:
+                                famMan.Set(parr, BamSym) 
+                                #print(str(BamSym))
                     
-                    elif str(pParr) == pfGU:
-                        FamSyms = FamId.GetFamilySymbolIds()
-                        print(FamSyms)
-                        for FamSym in FamSyms:
-                            famMan.Set(parr, (FamSym)) 
-                            
+                        elif str(pParr) == pfGU:
+                            FamSyms = FamId.GetFamilySymbolIds()
+                            #print(FamSyms)
+                            for FamSym in FamSyms:
+                                famMan.Set(parr, (FamSym)) 
 
-            trans.Commit()
+        #udelete unused nested families
+                    FlamSyms = FlamId.GetFamilySymbolIds()
+                    #print(FlamSyms)
+                    for FlamSym in FlamSyms: 
+                        FlamSId = family_temp.GetElement(FlamSym)
+#                    if FlamSId.Family.
+ #                       if not FlamSId.IsActive:
+ #                               family_temp.Delete(FlamSym)
+#purge nested families                                 
+                #purge nested prototypical type
+                  #set delete type as current type
+                famMan.CurrentType = deleteType
+                typeDel = famMan.DeleteCurrentType()  
+                if typeDel:
+                    print("Deleting embrionic type...")
+                trans.Commit()
 
-        else:
-            print ("88")
+        #else:
+            #print ("88")
+            #trans.RollBack()
+                    #if it doesn't work, throw an error message 
+        except Exception as e: 
+            print("Error: {}".format(e))
             trans.RollBack()
 
 
+    #do the pruge here
+
 #delete Delete family
+  
 
 #save as the family with new name and path
     family_temp.SaveAs(family_path, DB.SaveAsOptions())
-
+    print("saving new file...")
 # Load the saved family back into the project
+    print("loading new family into project...")
     with Transaction(doc, 'Load Family') as trans:
         trans.Start()
         family_loaded= doc.LoadFamily(family_path)
@@ -128,7 +158,7 @@ def save_as_new_family(door, family_name, panel_type, frame_type, width, height)
             print("Failed to load family.")
             return
         trans.Commit()
-
+    print ("reticulating splines...")
     # Clean up the temporary directory
     os.remove(family_path)
     os.rmdir(backupf_path)
@@ -258,8 +288,15 @@ def call_purge(family_name):
         print("Family not found with the specified name.")
 
                 
+def purgeIt():
 
-    
+    """Call Revit "Purge Unused" after completion."""
+    cid_PurgeUnused = \
+        UI.RevitCommandId.LookupPostableCommandId(
+            UI.PostableCommand.PurgeUnused
+            )
+    __revit__.PostCommand(cid_PurgeUnused)
+
 # Main function for user input etc
 def main():
     # Prompt user to select a door
@@ -275,11 +312,11 @@ def main():
 
     panel_type = "DF"
     #forms.ask_for_string("Enter Panel Type")
-    frame_type = "S01"
+    frame_type = "S02"
     #forms.ask_for_string("Enter Frame Type")
     width = 37
     #forms.ask_for_string("Enter Width (in inches)")
-    height = 84
+    height = 85
     #forms.ask_for_string("Enter Height (in inches)")
     # Define the new family name
     family_name = str.format(("08-Door-") + panel_type + ("-") + frame_type +("_HOK_I"))
